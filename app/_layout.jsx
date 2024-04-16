9b4c310dc1d4f3f70967b1e303df89fd97634001
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-16 12:57:39 +0530

    remove: unused importing statement

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,5 @@
-import { Slot, Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Stack } from 'expo-router';
+
 
 export default function App() {
   return (
