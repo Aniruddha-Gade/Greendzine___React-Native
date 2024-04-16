9253f27d6bdff6e453ce5132838fbcb492f837a2
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-16 12:55:43 +0530

    remove: top bar for tabs

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -6,11 +6,9 @@ export default function App() {
   return (
     <>
       <Stack>
-        <Stack.Screen
-          name='index' options={{
-            headerShown: false
-          }}
-        />
+        <Stack.Screen name='index' options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       </Stack>
     </>
   );
